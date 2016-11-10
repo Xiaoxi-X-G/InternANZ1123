4eea6a05156825c34e27d8d5677e8c2df392dd51
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-10 23:16:37 +1100

    ck

--- ImportData.R
+++ ImportData.R
@@ -3,10 +3,19 @@ rm(list = ls())
 
 DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 
+
+##
 BalanceSheet <- read.delim(file = paste(DataPath, "/balance_sheet.tsv", sep = ""))
 DebtBalance <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
                           quote = "" ,
                           stringsAsFactors = F)
 
+Cashflow <- read.delim(file = paste(DataPath, "/cashflow.tsv", sep = ""),
+                                      quote = "" ,
+                                      stringsAsFactors = F)
+
+IncomeStatement <- read.delim(file = paste(DataPath, "/income_statement.tsv", sep = ""),
+                       quote = "" ,
+                       stringsAsFactors = F)
 
 Metrics <- read.delim(file = paste(DataPath, "/metrics.tsv", sep = ""))
