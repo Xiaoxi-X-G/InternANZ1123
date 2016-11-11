18ab4b4ef41a6aebf1adf3ddd62da7f01e7b880b
Xiaoxi Guo <xguo@time2work.com>
2016-11-11 17:07:00 +1100

    ck

--- interanz1123_1.sql
+++ interanz1123_1.sql
@@ -1,19 +1,34 @@
+/* 10449
 select distinct DB.companyId from debt_balance DB
 inner join cashflow CF
 on CF.capiq_company_id = DB.companyId
+*/
 
+/* 10661
 select distinct DB.companyId from debt_balance DB
 inner join income_statement INS
 on INS.capiq_company_id = DB.companyId
+*/
 
+/* 10448
+select distinct DB.companyId from debt_balance DB
+inner join balance_sheet BS
+on BS.capiq_company_id = DB.companyId
+inner join income_statement INS
+on INS.capiq_company_id = DB.companyId
+inner join cashflow CF
+on CF.capiq_company_id = DB.companyId
+*/
 
 select distinct DB.companyId from debt_balance DB
 inner join balance_sheet BS
 on BS.capiq_company_id = DB.companyId
-
+inner join 
 
 select distinct DB.companyId from debt_balance DB
 
 select distinct CF.capiq_company_id  from cashflow CF
+
+
 
 
