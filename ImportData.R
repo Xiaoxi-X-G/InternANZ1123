0662e14724a3886c7b883a9766b6b5bf50519622
Xiaoxi Guo <xguo@time2work.com>
2016-11-11 12:31:09 +1100

    ck

--- ImportData.R
+++ ImportData.R
@@ -1,1 +1,9 @@
 rm(list = ls())
+
+DataPath <- "C:/gxx/Database/internanz1123/debt_issuance"
+
+#Import Data
+DebtBalance <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
+                          header = T,
+                          stringsAsFactors = F,
+                          quote = "")
