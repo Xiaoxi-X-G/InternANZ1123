e7527237286a191e7eb26cd6f17e2be256fadf92
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-12 10:37:25 +1100

    ck

--- ImportData.R
+++ ImportData.R
@@ -2,25 +2,6 @@ rm(list = ls())
 
 DataPath <- "C:/gxx/Database/internanz1123/debt_issuance"
 
-#Import Data
-# ColType <- c("int", #CompanyID 
-#              "int", #componentID
-#              "POSIXct", #StartDate
-#              "chr",
-#              "chr",
-#              "POSIXct", #filingDate
-#              "POSIXct",
-#              "chr", #periodType
-#              "num",
-#              "num", #Rate
-#              "int",
-#              "int",
-#              "int", #Seniority
-#              "chr",
-#              "chr",
-#              "chr",
-#              "chr",
-#              "chr", "int")
 DebtBalance <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
                           header = T,
                           stringsAsFactors = F,
