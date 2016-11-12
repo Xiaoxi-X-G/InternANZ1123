da2cda9dfd945ea34fb74b56cf9e729d0bb5a704
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-12 11:06:12 +1100

    filter out un-useful data

---
+++ DebtIssue.R
@@ -0,0 +1,9 @@
+rm(list = ls())
+
+DataPath <- "C:/gxx/Database/internanz1123/debt_issuance"
+
+DebtBalance <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
+                          header = T,
+                          stringsAsFactors = F,
+                          quote = "",
+                          na.strings = c("","NA", "NULL"))
