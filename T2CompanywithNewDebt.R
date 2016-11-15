9f67e04db13706c0abad257e54cc828ec04b77f5
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-15 21:04:53 +1100

    add comment

--- T2CompanywithNewDebt.R
+++ T2CompanywithNewDebt.R
@@ -12,7 +12,7 @@ DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 
 # Step1: Import debt balance and filter out companies using the companyIDs shared by all files
 # Step2: Find new market entry dates of each companies
-
+# Step3: Rank the companies for each currency: A score in [0,1] is given for a company for each currency
 
 
 # ####1. Import debt_balance, debt_issuance ####
@@ -81,74 +81,78 @@ for (i in unique(DebtBalance2.order$companyId)){
   print(2401-m)
   
   RowInd <- which(DebtBalance2.order$companyId == i)
-  # UniqueComponent <- unique(DebtBalance2.order$componentId[RowInd])
-  # MatchInd1st <- match(UniqueComponent, DebtBalance2.order$componentId) 
   
-  #1 For each componentID of a company, find Currencies and the first dates
   matrix.currency <- c()
-  matrix.currency <- DebtBalance2.order[RowInd, ] 
+  matrix.currency <- DebtBalance2.order[RowInd, ] # all currencies and the issue dates (used by Period End Date)
   
-  #2 Find entry dates which is the date a compnay hasn't issued debts of a currency in last three years
-  matrix.currency.order <- c()
+  matrix.currency.order <- c() # ordered by types of currencies and PeriodEndDate
   matrix.currency.order <- matrix.currency[order(matrix.currency$IssuedCurrency,
                                                  matrix.currency$PeriodEndDate),]
   
-  #3.1 write down first enty date of all currency
+  #2.2 write down first entry date of all currencies
   MatchInd1st.currency <- match(unique(matrix.currency.order$IssuedCurrency), matrix.currency.order$IssuedCurrency)
   df.firstdate <- c()
   df.firstdate <- matrix.currency.order[MatchInd1st.currency,]
   
-  #3.2 if the same currency re-apprears, check if the difference is > 3 years
+  #2.3, if the same currency re-apprears, check if the difference is > 3 years
   for (j in unique(matrix.currency.order$IssuedCurrency)){
     RowInd.currency <- which(matrix.currency.order$IssuedCurrency == j)
     
     Ind.temp <- which(as.integer(diff(matrix.currency.order$PeriodEndDate[RowInd.currency])) > (365*3))
     
-    if (length(Ind.temp) > 0){
+    if (length(Ind.temp) > 0){ #2.3 if > 3 years, keep another record 
       df.firstdate<- rbind(df.firstdate, matrix.currency.order[RowInd.currency[Ind.temp+1],]) 
     }
   }
   colnames(df.firstdate)[[3]] <- "MarketEntryDate"
   df.firstdate <- df.firstdate[order(df.firstdate$IssuedCurrency, df.firstdate$MarketEntryDate),]
   
+  #2.4,, combiningn data 
   MarketEntryData <- rbind(MarketEntryData, df.firstdate)
 }
 
 
-######Rank ######
+######Step3 ######
+#### Rank the companies for each currency: A score in [0,1] is given for a company for each currency
+
+
+#3.1: Order all companyID by Currencies, and by Market Entry Date
 CompanyScore.temp <- MarketEntryData[order(MarketEntryData$IssuedCurrency,
                                            MarketEntryData$MarketEntryDate,
                                            MarketEntryData$companyId),
                                      ]
 
-
-# score each companies across all currencies
-# i.e., CompnayID AED AMD AOA ARS AUD AZN BDT BEF BGN
-
+#3.2 score each company across all currencies
 CompanyScore <- matrix(0, nrow = length(unique(CompanyScore.temp$companyId)),
                        ncol = (1+ length(unique(CompanyScore.temp$IssuedCurrency))))
 
 
-colnames(CompanyScore) <- c("CompnayID",  
+colnames(CompanyScore) <- c("CompanyID",  
                             unique(CompanyScore.temp$IssuedCurrency) )
 CompanyScore[,1] <- unique(CompanyScore.temp$companyId)
 
 
 for (i in unique(CompanyScore.temp$IssuedCurrency)){
-  RowInd <- which(CompanyScore.temp$IssuedCurrency == i) # for each currency find all records
+  #3.2.1: For each currency find all records
+  RowInd <- which(CompanyScore.temp$IssuedCurrency == i) # 
+  temp11 <-c()
   temp11 <- CompanyScore.temp[RowInd, c(1,3)]
-  rank.temp <- rank(-as.integer(temp11$MarketEntryDate))
-  temp11$rank <- rank.temp/max(rank.temp) # rank and score based on debts issue date, normalize to [0,1]
   
-  Score <- tapply(temp11$rank, INDEX = temp11$companyId, FUN = sum)
+  #3.2.2: Rank and  give a score based on debts issue date
+  rank.temp <- rank(-as.integer(temp11$MarketEntryDate))  
+  temp11$Rank <- rank.temp/max(rank.temp) # normalize score to [0,1]
+  
+  #3.2.3: If a company issued multiple currencies, add-up the score 
+  Score <- tapply(temp11$Rank, INDEX = temp11$companyId, FUN = sum) 
   RowInd.score <- match(names(Score), CompanyScore[,1]) # Find the companyID index for the given currency
   
-  CompanyScore[RowInd.score, i] <- unname(Score)
+  CompanyScore[RowInd.score, i] <- unname(Score) #
 }
 
+#3.3: Find the total score of each company, based on which to order companyID
 df.CompanyScore <- data.frame(CompanyScore)
 df.CompanyScore$TotalScore <- rowSums(df.CompanyScore[,-1])
 
-Finial.CompanyScore <- df.CompanyScore[order(df.CompanyScore$TotalScore, decreasing=T),]
+Finial.CompanyScore <- df.CompanyScore[order(df.CompanyScore$TotalScore, decreasing = T),]
 
-plot(sort(Finial.CompanyScore$USD, decreasing =T))
+plot(sort(Finial.CompanyScore$TotalScore, decreasing =T))
