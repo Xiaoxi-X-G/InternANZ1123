8c7f2cc59f831374bc2a31f8c505be354c6c5ff0
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-15 20:20:57 +1100

    update filter

--- T1FilterFiles.R
+++ T1FilterFiles.R
@@ -8,7 +8,7 @@ DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 ##########################################################
 
 
-####1. Find CompanyIDs by inner join all tables####
+####1. Find CompanyIDs by inner join  cashflow, balance_sheet, income_statement####
 
 library(RODBC)
 odbcDataSources()
@@ -32,39 +32,45 @@ CompanyID.Common <- sqlQuery(conn, "select distinct CF.capiq_company_id AS DebtI
 
 ####2. Import "CompanyIDComm" to SQL #####
 
-####3. Filter all other fills  #####
-Cashflow.2use <- sqlQuery(conn, " select * from cashflow CF
+####3. Filter all other fills, CompanyIDComm & Balance_Issuance  #####
+Cashflow.2use <- sqlQuery(conn, "select * from cashflow CF
  where CF.capiq_company_id in
                           (
-                          select companyid from CompanyIDComm
+                          	select distinct CIC.CompanyID from CompanyIDComm CIC
+	inner join  DebtIssuance_Short DIS
+                          on CIC.CompanyID = DIS.CompanyID
                           )",
                           stringsAsFactors = FALSE, as.is = TRUE, 
                           na.string = c("NULL", "NA", ""))
 
-# write.csv(Cashflow.2use, paste(DataPath, "/Cashflow_Filtered.csv", sep = ""),
+# write.csv(Cashflow.2use, paste(DataPath, "/Cashflow_Filtered2.csv", sep = ""),
 #           row.names = F, quote =  F)
 
 ###################
 
-IncomeStatement.2use <- sqlQuery(conn, "select * from income_statement CF where CF.capiq_company_id in 
+IncomeStatement.2use <- sqlQuery(conn, "select * from income_statement CF  where CF.capiq_company_id in
                           (
-                                 select companyid from CompanyIDComm
+                                 select distinct CIC.CompanyID from CompanyIDComm CIC
+                                 inner join  DebtIssuance_Short DIS
+                                 on CIC.CompanyID = DIS.CompanyID
 )",
                           stringsAsFactors = FALSE, as.is = TRUE, 
                           na.string = c("NULL", "NA", ""))
 
-# write.csv(IncomeStatement.2use, paste(DataPath, "/IncomeStatement_Filtered.csv", sep = ""),
+# write.csv(IncomeStatement.2use, paste(DataPath, "/IncomeStatement_Filtered2.csv", sep = ""),
 #           row.names = F, quote =  F)
 
 ######################
 
-BalanceSheet.2use <- sqlQuery(conn, "select * from balance_sheet CF where CF.capiq_company_id in 
-                              (
-                              select companyid from CompanyIDComm
-)",
+BalanceSheet.2use <- sqlQuery(conn, "select * from balance_sheet CF  where CF.capiq_company_id in
+                          (
+                          	select distinct CIC.CompanyID from CompanyIDComm CIC
+	inner join  DebtIssuance_Short DIS
+                          on CIC.CompanyID = DIS.CompanyID
+                          )",
                           stringsAsFactors = FALSE, as.is = TRUE, 
                           na.string = c("NULL", "NA", ""))
 
-# write.csv(BalanceSheet.2use, paste(DataPath, "/BalanceSheet_Filtered.csv", sep = ""),
+# write.csv(BalanceSheet.2use, paste(DataPath, "/BalanceSheet_Filtered2.csv", sep = ""),
 #           row.names = F, quote =  F)
 
