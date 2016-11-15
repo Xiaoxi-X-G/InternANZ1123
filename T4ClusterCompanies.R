5e27735d46c517cb1e837df08b6a93053b6821ba
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-15 21:28:51 +1100

    ck

--- T4ClusterCompanies.R
+++ T4ClusterCompanies.R
@@ -2,8 +2,7 @@ rm(list = ls())
 getMKLthreads()
 DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 
-######################Task 3 ############################
-#3. Using "market_entries", rank companies based on the timing 
-# of the entrance into that market (this rank will be your 
-# leadership score
+######################Task 4 ############################
+#4. Cluster companies based on their financial metrics, 
+# so that there is a spread of leaders and followers
 ##########################################################
