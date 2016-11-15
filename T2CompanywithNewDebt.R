80853754c51fd40306f0c944452529b8e2213233
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-15 20:21:16 +1100

    add comments

--- T2CompanywithNewDebt.R
+++ T2CompanywithNewDebt.R
@@ -10,43 +10,47 @@ DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 ##########################################################
 
 
-####Import debt_balance, debt_issuance ####
-DebtBalance.raw <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
-                              header = T,
-                              stringsAsFactors = F,
-                              quote = "",
-                              na.strings = c("","NA", "NULL"))
+# Step1: Import debt balance and filter out companies using the companyIDs shared by all files
+# Step2: Find new market entry dates of each companies
 
-DebtIssuance.raw <- read.delim(file = paste(DataPath, "/debt_issuance.rpt", sep = ""),
-                               header = T,
-                               stringsAsFactors = F,
-                               quote = "",
-                               na.strings = c("","NA", "NULL"))
+
+
+# ####1. Import debt_balance, debt_issuance ####
+# DebtBalance.raw <- read.delim(file = paste(DataPath, "/debt_balance.tsv", sep = ""),
+#                               header = T,
+#                               stringsAsFactors = F,
+#                               quote = "",
+#                               na.strings = c("","NA", "NULL"))
 # 
-# write.csv(DebtIssuance.raw[, c(1)], paste(DataPath, "/DebtIssuance_Short.csv", sep = ""),
-#           row.names = F, quote =  F)
+# DebtIssuance.raw <- read.delim(file = paste(DataPath, "/debt_issuance.rpt", sep = ""),
+#                                header = T,
+#                                stringsAsFactors = F,
+#                                quote = "",
+#                                na.strings = c("","NA", "NULL"))
+# # 
+# # write.csv(DebtIssuance.raw[, c(1)], paste(DataPath, "/DebtIssuance_Short.csv", sep = ""),
+# #           row.names = F, quote =  F)
 
 
-CompanyIDComm<- read.csv(file = paste(DataPath, "/CompanyIDComm.csv", sep=""), 
-                         header = T,
-                         stringsAsFactors = F)
+# CompanyIDComm<- read.csv(file = paste(DataPath, "/CompanyIDComm.csv", sep=""), 
+#                          header = T,
+#                          stringsAsFactors = F)
 
 
 
-
-### filter debt balance: using companyIDs shared by all files
+##### Step 1 #####
+##### Import debt balance and filter out companies using the companyIDs shared by all files
 library(RODBC)
 odbcDataSources()
 conn<-odbcConnect(dsn="localdb") 
 
-DebtBalance2 <- 
-  sqlQuery(conn, "select DB.companyId,   convert(char(10),DB.filingDate, 126) AS filingDate, 
+DebtBalance2 <- sqlQuery(conn, "select DB.companyId,   convert(char(10),DB.filingDate, 126) AS filingDate, 
                          convert(char(10), DB.periodEndDate,  126) AS PeriodEndDate, 
-                         DB.issuedCurrency AS IssuedCurrency from interanz1123.dbo.debt_balance DB
+                         DB.issuedCurrency AS IssuedCurrency from internanz1123.dbo.debt_balance DB
                          where DB.companyId in 
                          (
-                         select distinct CIC.CompanyID from interanz1123.dbo.companyIDComm CIC
-                         inner join interanz1123.dbo.DebtIssuance_Short DIS
+                         select distinct CIC.CompanyID from internanz1123.dbo.companyIDComm CIC
+                         inner join internanz1123.dbo.DebtIssuance_Short DIS
                          on CIC.companyId = DIS.CompanyID
                          )
                          order by DB.companyId, DB.issuedCurrency, DB.periodEndDate
@@ -56,25 +60,21 @@ DebtBalance2 <-
 
 
 DebtBalance2$companyId <- as.integer(DebtBalance2$companyId)
-
 DebtBalance2$filingDate <- as.Date(DebtBalance2$filingDate)
 DebtBalance2$PeriodEndDate <- as.Date(DebtBalance2$PeriodEndDate)
-
 
 DebtBalance2.order <- DebtBalance2[order(DebtBalance2$companyId,
                                          DebtBalance2$IssuedCurrency,
                                          DebtBalance2$PeriodEndDate),]
 
 
-#### Find  dates ####
-# Remove rows where issuedCurrency = NA, around 2.6% of the rows
+#####Step 2####
+#### Find new market entry dates of each companies
+
+#2.1 Remove rows where issuedCurrency = NA, around 3.3% of the rows
 DebtBalance2.order <- DebtBalance2.order[-which(is.na(DebtBalance2.order$IssuedCurrency)),]
 
-ColInd <- c(1,2,4,6, 7) #"CompanyID", "filingDate", ""PeriodEndDate", issuedCurrency
-
-
 MarketEntryData <- data.frame()
-m <-0
 m <-0
 for (i in unique(DebtBalance2.order$companyId)){
   m<-m+1
@@ -85,14 +85,17 @@ for (i in unique(DebtBalance2.order$companyId)){
   # MatchInd1st <- match(UniqueComponent, DebtBalance2.order$componentId) 
   
   #1 For each componentID of a company, find Currencies and the first dates
+  matrix.currency <- c()
   matrix.currency <- DebtBalance2.order[RowInd, ] 
   
   #2 Find entry dates which is the date a compnay hasn't issued debts of a currency in last three years
+  matrix.currency.order <- c()
   matrix.currency.order <- matrix.currency[order(matrix.currency$IssuedCurrency,
                                                  matrix.currency$PeriodEndDate),]
   
   #3.1 write down first enty date of all currency
   MatchInd1st.currency <- match(unique(matrix.currency.order$IssuedCurrency), matrix.currency.order$IssuedCurrency)
+  df.firstdate <- c()
   df.firstdate <- matrix.currency.order[MatchInd1st.currency,]
   
   #3.2 if the same currency re-apprears, check if the difference is > 3 years
