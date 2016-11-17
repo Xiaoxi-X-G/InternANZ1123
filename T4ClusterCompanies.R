8c46707187f7229c8088abae06112f2b37cff0a6
Xiaoxi <pluto.xiaoxi@gmail.com>
2016-11-17 21:29:35 +1100

    output dashboard data

--- T4ClusterCompanies.R
+++ T4ClusterCompanies.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 getMKLthreads()
-DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
+DataPath <- "C:/gxx/Database/internanz1123/debt_issuance"
 
 ######################Task 4 ############################
 #4. Cluster companies based on their financial metrics, 
@@ -9,7 +9,10 @@ DataPath <- "C:/DataAnalysis/Data/anz_debt_issuance"
 
 
 
-# Import financial data
+##### Step1 ####
+# Import financial data, and combine into single file
+
+### 1.1 Import Data
 CompanyScore.Finial <- read.csv(paste(DataPath, "/FinialCompanyScore.csv", sep=""),
                                 stringsAsFactors = F)
 IncomeStatement <- read.csv(paste(DataPath, "/IncomeStatement_Filtered3.csv", sep=""),
@@ -21,15 +24,8 @@ Cashflow <- read.csv(paste(DataPath, "/Cashflow_Filtered3.csv", sep=""),
 
 
 
-### Check####
-CommCol <- intersect(intersect(unique(names(IncomeStatement)),unique(names(BalanceSheet))), 
-                     unique(names(Cashflow)))
-
-ncol(IncomeStatement) + ncol(BalanceSheet) + ncol(Cashflow)
-
-
-### For each ID check if there are repeated EndofPeriod
-ToCheck <- Cashflow
+### 1.2 For each CompanyID, check if there are repeated Period --- NO
+ToCheck <- IncomeStatement
 
 ID2Check <- c()
 for (m in unique(ToCheck$capiq_company_id)){
@@ -40,20 +36,15 @@ for (m in unique(ToCheck$capiq_company_id)){
   }
 }
 
-Inds <- which(duplicated(ToCheck$period_end_date[Rows]))
 
-ToCheck$period_end_date[Rows[Inds]]
 
-ToCheck[Rows,c(1:5)]
-
-#### Combine all Data ####
+###1.3 Combine Data, using CompanyID and Period
 BalanceSheet$period <- as.numeric(substr(BalanceSheet$period, start = 3, stop = 8))
 Cashflow$period <- as.numeric(substr(Cashflow$period, start = 3, stop = 8))
 IncomeStatement$period <- as.numeric(substr(IncomeStatement$period, start = 3, stop = 8))
 
 AllCol <- sort(union(union(names(BalanceSheet), names(IncomeStatement)), names(Cashflow)))
 CommCompanyID <- unique(CompanyScore.Finial$CompanyID)
-
 
 AllFinancialData <- data.frame()
 n <-0
@@ -67,11 +58,11 @@ for (m in CommCompanyID){
   
   # Find all Dates, where a company has published a report
   AllDate <- sort(unique(c(BalanceSheet$period[RowInd.BS], 
-                                   Cashflow$period[RowInd.CF], 
-                                   IncomeStatement$period[RowInd.IS])))
+                           Cashflow$period[RowInd.CF], 
+                           IncomeStatement$period[RowInd.IS])))
   
   # Combine all data of a company
-
+  
   AllData.temp <- matrix(NA, nrow=length(AllDate), ncol = length(AllCol))
   rownames(AllData.temp) <- c(as.character(AllDate))
   colnames(AllData.temp) <- AllCol
@@ -92,66 +83,102 @@ for (m in CommCompanyID){
   
   AllFinancialData <- rbind(AllFinancialData, AllData.temp)
 }
-# 
-write.csv(AllFinancialData, paste(DataPath, "/AllFinancialData.csv", sep=""),
+
+# write.csv(AllFinancialData, paste(DataPath, "/AllFinancialData.csv", sep=""),
+#           quote = F, row.names = F)
+
+
+AllFinancialData <- read.csv(paste(DataPath, "/AllFinancialData.csv", sep=""),
+                             stringsAsFactors = F)
+
+####Step2#######
+### Delete duplicated columns, and columns with high correlation  
+library(caret)
+
+AllFinancialData.filtered <- 
+  AllFinancialData[, c(1: which(colnames(AllFinancialData)=="period"))]
+
+AllFinancialData.filtered$filing_date <- 
+  as.integer(as.Date(AllFinancialData.filtered$filing_date))
+
+AllFinancialData.filtered <- AllFinancialData.filtered[, -c(2:6)]
+
+correlationMatrix <- cor(AllFinancialData.filtered, 
+                         use = "pairwise.complete.obs")
+
+AllFinancialData.filtered2 <- 
+  AllFinancialData.filtered[-findCorrelation(correlationMatrix, cutoff=0.75)] 
+
+AllFinancialData.filtered2.order <- 
+  cbind(AllFinancialData.filtered2[, ncol(AllFinancialData.filtered2)],
+        AllFinancialData.filtered2[, c(1: (ncol(AllFinancialData.filtered2)-1))])
+
+colnames(AllFinancialData.filtered2.order)[[1]] <- "period"
+
+# write.csv(AllFinancialData.filtered2.order, paste(DataPath, "/AllFinancialData.filtered2.order.csv", sep=""),
+#           quote = F, row.names = F)
+
+
+AllFinancialData.filtered2.order <- 
+  read.csv(paste(DataPath, "/AllFinancialData_filtered2_order.csv", sep= ""), stringsAsFactors = F)
+
+### check, common columns  
+length(intersect(colnames(Cashflow),colnames(AllFinancialData.filtered2)))
+length(intersect(colnames(BalanceSheet),colnames(AllFinancialData.filtered2)))
+length(intersect(colnames(IncomeStatement),colnames(AllFinancialData.filtered2)))
+
+
+## highest score
+AllFinancialData.filtered2.order[which(AllFinancialData.filtered2.order$capiq_company_id == "3648306"),]
+
+## lowest score
+AllFinancialData.filtered2.order[which(AllFinancialData.filtered2.order$capiq_company_id == "3116362"),]
+
+
+
+### Re-design matrix2 for display in Tableau
+
+CompanyScore.Finial.Redesinged <- data.frame()
+yy <- 0
+for (w in 1:nrow(CompanyScore.Finial)){
+  yy <- yy + 1 
+  IndName <- colnames(CompanyScore.Finial)[which(CompanyScore.Finial[w,] > 0)]
+  IndValue <- CompanyScore.Finial[w, IndName]
+  lg <- length(IndValue)
+  df.temp <- data.frame(CompanyID = rep(as.integer(unname(IndValue[1])), (lg-1)), 
+                        Currency = tail(names(IndValue), n =(lg-1)), 
+                        Score= tail(as.numeric(unname(IndValue)), n =(lg-1)),
+                        stringsAsFactors = F)
+  
+  CompanyScore.Finial.Redesinged<-rbind(CompanyScore.Finial.Redesinged, df.temp)
+  
+}
+# write.csv(CompanyScore.Finial.Redesinged, paste(DataPath, "/CompanyScore_Finial_Redesinged.csv", sep=""),
+#           quote = F, row.names = F)
+
+
+
+### Re-design matrix2 for display in Tableau
+AllFinancialData.filtered2.order.redesign <- data.frame()
+v <- 0
+for (v in 1:nrow(AllFinancialData.filtered2.order)){
+  v <- v+1
+  print(121070-v)
+  
+  IndName2 <- 
+    colnames(AllFinancialData.filtered2.order)[which(!is.na(AllFinancialData.filtered2.order[v,]) )]
+  
+  IndValue2 <- AllFinancialData.filtered2.order[v, IndName2]
+  lg2 <- length(IndValue2)
+  df.temp2 <- data.frame(CompanyID = rep(as.integer(unname(IndValue2[2])), (lg2-2)),
+                         Period = rep(as.numeric(unname(IndValue2[1])), (lg2-2)),
+                         Terms = tail(names(IndValue2), n =(lg2-2)) ,
+                         Value =  tail(as.numeric(unname(IndValue2)), n =(lg2-2)),
+                         stringsAsFactors = F)
+  
+  AllFinancialData.filtered2.order.redesign <- rbind(AllFinancialData.filtered2.order.redesign,
+                                                     df.temp2)
+}
+write.csv(AllFinancialData.filtered2.order.redesign, 
+          paste(DataPath, "/AllFinancialData.filtered2.order.redesign.csv", sep=""),
           quote = F, row.names = F)
-
-head(AllFinancialData[, c(1:5, 200:206)])
-
-
-Percentage <- 0.3 # Keep columns where the number of NAs < 30%
-InputData <- AllFinancialData
-
-NACols <- apply(InputData, MARGIN = 1, function(x, y=nrow(InputData)){
-  length(which(is.na(x)))/y}
-)
-#
-KeepInd <- match(names(NACols[which(NACols < Percentage)]), names(InputData))
-
-
-
-
-BalanceSheet[head(which(BalanceSheet$capiq_company_id==21671), n =30), CommCol]
-
-
-
-
-IncomeStatement[head(which(IncomeStatement$capiq_company_id==21671), n =30), CommCol]
-
-Cashflow[which(Cashflow$capiq_company_id==21671), CommCol]
-
-
-
-# c(2:8)
-# 
-# head(tail(c(1:ncol(InputData)), n = 4),2)
-# 
-# 
-# 
-# #########
-# InputData <- Cashflow
-# 
-# head(colnames(InputData), n =10)
-# tail(colnames(InputData), n =10)
-# 
-Percentage <- 0.3 # Keep columns where the number of NAs < 30%
-
-
-NACols <- apply(InputData, MARGIN = 2, function(x, y=nrow(InputData)){
-  length(which(is.na(x)))/y}
-)
-#
-KeepInd <- match(names(NACols[which(NACols < Percentage)]), names(InputData))
-# 
-# InputData.Keep.temp <- InputData[,KeepInd]
-# InputData.Keep <- InputData.Keep.temp[, -c(2:8)]
-# 
-# ###
-# Col2Keep <- function(InputData, Percentage){
-#   NACols <- apply(InputData, MARGIN = 2, function(x, y=nrow(InputData)){ 
-#     length(which(is.na(x)))/y}
-#   )
-#   
-#   KeepInd <- match(names(NACols[which(NACols < Percentage)]), names(InputData))
-#   return(InputData[,KeepInd])
-# }
